805146011292ef0a897bb799b226d6b65abb1330
charliejuc <charliejuc@gmail.com>
2020-05-25 15:43:12 +0200

    Default direct exchange pub/sub.
    new file:   .gitignore
    new file:   package.json
    modified:   publisher/index.js
    modified:   subscriber/index.js
    new file:   yarn.lock

--- publisher/index.js
+++ publisher/index.js
@@ -1,1 +1,28 @@
 'use strict'
+
+const amqp = require('amqplib/callback_api')
+const queue = process.env.QUEUE || 'hello'
+
+amqp.connect('amqp://localhost', function(error, connection) {
+  if (error) {  
+    throw error
+  }
+  console.log('CONNECTED')
+
+  connection.createChannel(function(error, channel) {
+    if (error) {
+      throw error
+    }
+
+    const message = 'Test world! ' + Math.random().toString(32).slice(2)
+
+    // channel.deleteQueue(queue, {}, console.log)
+
+    channel.assertQueue(queue, {
+      durable: false
+    })
+
+    channel.sendToQueue(queue, Buffer.from(message))
+    console.log(" [x] Sent %s", message)
+  })
+})
--- subscriber/index.js
+++ subscriber/index.js
@@ -1,1 +1,33 @@
 'use strict'
+
+const amqp = require('amqplib/callback_api')
+const queue = process.env.QUEUE || 'hello'
+
+amqp.connect('amqp://localhost', function(error, connection) {
+  if (error) {  
+    throw error
+  }
+  console.log('CONNECTED')
+
+  connection.createChannel(function(error, channel) {
+    if (error) {
+      throw error
+    }
+
+    channel.assertQueue(queue, {
+      durable: false
+    })
+
+    channel.consume(queue, message => {
+      // console.log(message)
+      console.log(queue, 'START')
+
+      let i = 2e9
+      while(--i) {}
+
+      console.log(" [x] Received %s", message.content.toString())
+    }, {
+      noAck: true
+    })
+  })
+})
