e1a9c4b74c188a7f071f9c3109b1d16dc9541a14
charliejuc <charliejuc@gmail.com>
2020-06-01 06:52:24 +0200

    Format with prettier.

--- publisher/index.js
+++ publisher/index.js
@@ -7,53 +7,54 @@ const messagesAmount = 6
 const wait = 400
 
 function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms)
+    })
 }
 
 async function sleepLoop(number, cb) {
-  while(number--) {
-    await sleep(wait)
+    while (number--) {
+        await sleep(wait)
 
-    cb()
-  }
+        cb()
+    }
 }
 
 async function exitAfterSend() {
-  await sleep(messagesAmount * wait * 1.2)
+    await sleep(messagesAmount * wait * 1.2)
 
-  process.exit(0)
+    process.exit(0)
 }
 
-async function subscriber () {
-  const connection = await amqp.connect('amqp://localhost')
-  const channel = await connection.createChannel()
+async function subscriber() {
+    const connection = await amqp.connect('amqp://localhost')
+    const channel = await connection.createChannel()
 
-  await channel.assertQueue(queue)
+    await channel.assertQueue(queue)
 
-  sleepLoop(messagesAmount, async () => {
-    const message = {
-      id: Math.random().toString(32).slice(2, 6),
-      text: 'Hello world!'
-    }
-  
-    const sent = await channel.sendToQueue(queue, Buffer.from(
-      JSON.stringify(message)
-    ), {
-      // persistent: true
+    sleepLoop(messagesAmount, async () => {
+        const message = {
+            id: Math.random().toString(32).slice(2, 6),
+            text: 'Hello world!'
+        }
+
+        const sent = await channel.sendToQueue(
+            queue,
+            Buffer.from(JSON.stringify(message)),
+            {
+                // persistent: true
+            }
+        )
+
+        sent
+            ? console.log(`Sent message to "${queue}" queue`, message)
+            : console.log(`Fails sending message to "${queue}" queue`, message)
     })
-  
-    sent ?
-      console.log(`Sent message to "${queue}" queue`, message) :
-      console.log(`Fails sending message to "${queue}" queue`, message)
-  })
 }
 
-subscriber()
-  .catch(error => {
+subscriber().catch((error) => {
     console.error(error)
     process.exit(1)
-  })
+})
 
 exitAfterSend()
