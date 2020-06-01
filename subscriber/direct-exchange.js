4f0f9d7a5a31bb87031080fbb535cba147ae6e4c
charliejuc <charliejuc@gmail.com>
2020-06-01 17:18:14 +0200

    Direct exchange example.

---
+++ subscriber/direct-exchange.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const amqp = require('amqplib')
+const queue = process.env.QUEUE || 'hello'
+const exchangeName = process.env.EXCHANGE || 'my-direct'
+const exchangeType = 'direct'
+
+console.log({
+    queue,
+    exchangeName,
+    exchangeType
+})
+
+function intensiveOperation() {
+    let i = 1e3
+    while (i--) {}
+}
+
+async function subscriber() {
+    const connection = await amqp.connect('amqp://localhost')
+    const channel = await connection.createChannel()
+
+    await channel.assertQueue(queue)
+
+    await channel.assertExchange(exchangeName, exchangeType)
+
+    await channel.bindQueue(queue, exchangeName, 'a')
+
+    channel.consume(queue, (message) => {
+        const content = JSON.parse(message.content.toString())
+
+        intensiveOperation()
+
+        console.log(`Received message from "${queue}" queue`)
+        console.log(content)
+
+        channel.ack(message)
+    })
+}
+
+subscriber().catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
